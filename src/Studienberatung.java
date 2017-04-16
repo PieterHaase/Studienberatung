cde70b984c1182eb6600cc0ba423f5b63d347d71
Pieter Haase <pieter.haase@haw-hamburg.de>
2017-04-16 14:03:31 +0200

    added main method

--- src/Studienberatung.java
+++ src/Studienberatung.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
 
 public class Studienberatung {
+	
+	public Studienberatung(){
+		Scanner scan = new Scanner(System.in);
+	}
+	
+	public static void main (String args[]){
+		new Studienberatung();
+	}
 
 }
