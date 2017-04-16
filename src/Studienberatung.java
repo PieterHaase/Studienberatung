43ed76c05b25ac1f89eb4505c5d82d0a93269225
Pieter Haase <pieter.haase@haw-hamburg.de>
2017-04-16 14:07:17 +0200

    added scanner input

--- src/Studienberatung.java
+++ src/Studienberatung.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Studienberatung {
 	
+	private String input;
+	
 	public Studienberatung(){
 		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Hast du Abitur?");
+		input = scan.nextLine();
 	}
 	
 	public static void main (String args[]){
